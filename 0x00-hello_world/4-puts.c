dfa7edb6a4cf0b22cf952d53e0380075d0b6e986
enny2004 <omoyemieniola22@gmail.com>
2022-09-10 11:12:29 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - printing C
  *
- * 	Return: Always 0 (successful C)
+ * Return: Always 0 (successful C)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
- return (0);
+ 	return (0);
 }	
