ec42290ea882498a9f31620ee72ff627e064008f
enny2004 <omoyemieniola22@gmail.com>
2022-09-10 09:17:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
- #include <stdio.h>
- /**
-  * main - printing C
-  *
-  * Return: Always 0 (successful C)
-  */
- int main(void)
- {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
- } 
+#include <stdio.h>
+/**
+ * main - printing C
+ *
+ * Return: Always 0 (successful C)
+ */
+int main(void)
+{
+puts("/"programming is like building a multilingual puzzle");       
+return (0);
+}	
