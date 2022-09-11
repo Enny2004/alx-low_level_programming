03b2b8c45ed6b0f7f9c6c976bdf4f04a3a2157d6
enny2004 <omoyemieniola22@gmail.com>
2022-09-11 04:50:51 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
- *main-entry point
+ *main-Coding c
  * 
  *Return:return zero if no error
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
+	
 	return (0);
 }
