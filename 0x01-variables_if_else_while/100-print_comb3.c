f0a88300e975b86b387e7ca2423c24051bb234ea
enny2004 <omoyemieniola22@gmail.com>
2022-09-12 21:50:54 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main-print all possible wo digit
+ *
+ * Return:Always (sucessful)
+ */
+int main(void)
+(
+	int i, j;
+
+	for (i = 0; j <= 9; i++)
+	{
+	
+		for (i = 0; i <= 9; i++)
+		if (j > i)
+
+		{
+			pucwq!
+			thar(i + '0');
+
+			putchar(j + '0');
+
+			if (i != 8)
+			{
+				putchar(',');
+			
+				putchar(' ');
+			}
+
+
+
+		}
+
+		putchar('\n');
+
+		return (0);
+		
+	}
