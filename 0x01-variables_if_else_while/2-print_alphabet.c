4f893babb4e01e734954204ee6aae77853ad72b8
enny2004 <omoyemieniola22@gmail.com>
2022-09-12 17:35:18 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main-lowercase alphabets
+ *
+ * Return;Always(success)
+ */
+int main9void0
+{
+	char c;
+
+	for (c = 'a';c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
