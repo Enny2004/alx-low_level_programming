9add9c39fc31f54be07ea0ecec745b7e1feab671
enny2004 <omoyemieniola22@gmail.com>
2022-09-14 04:38:32 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,19 +6,18 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
+	int i
 
-	for (i = 0; i < 10; i++)
-	{
-		int j;
+		for (i = 0; i < 10; i++)
 
-		for (j = 'a'; j <= 'z'; j++)
+		{
+			int j
 
-	{
-		_putchar(j);
-	}
-	
-	_putchar('\n');
+				for (j = 'a'; j <= 'z'; j++)
+				{
+					_putchar(j);
+				}
 
-	}
+			putchar('\n');
+		}
 }
