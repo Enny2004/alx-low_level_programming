51184974b5c52610f3a3544fd8b799a5108aec80
enny2004 <omoyemieniola22@gmail.com>
2022-09-14 03:38:16 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main-printing the alphabet in lower case 
+ *
+ * Return:Always (sucessful)
+ */
+int main(void)
+{
+	int c
+	
+	printf("void print_alphabet");
+
+	for (c = a; c >= z; c++);
+
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return(0);
+}
