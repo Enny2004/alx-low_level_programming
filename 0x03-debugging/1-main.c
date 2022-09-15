6847e35c908e92e3c91f15762d0d4557b55bb763
enny2004 <omoyemieniola22@gmail.com>
2022-09-15 15:02:54 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,6 +9,7 @@ int main(void)
 {
 	int i;
 	printf("infinite loop incoming :(\n");
+
 	i=0;
 	/**
 	 * while (i < 10)
@@ -16,7 +17,8 @@ int main(void)
 	 *	putchar(i);
 	 * }
 	 */
-		printf("infinite ioop avoided! \\o/\n");
 
-		return(0);
+	printf("infinite ioop avoided! \\o/\n");
+
+	return(0);
 }
