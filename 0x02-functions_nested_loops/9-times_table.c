4f9798733b90b108160d00dd25ef5641c89ff786
enny2004 <omoyemieniola22@gmail.com>
2022-09-14 18:06:30 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,10 +7,33 @@
 void times_table(void)
 
 {
-	int ch;
-	for (ch = 0; ch <= 10; ch++)
-	_putchar ("%d * %d = %d",numbder,counter,number*counter)
+	int x, y, k;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+
+		{
+			k = x * y;
+			if (y == 0)
+			_putchar(k + '0');
+			if(y != 0 && k < 10)
+			{
+				_putchar(',');
+				_putchar('');
+				_putchar('');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar('');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
 
 		_putchar('\n');
+	}
 }
 
