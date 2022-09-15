a260e33d5b54689ca05426c5fec86e5a6da34f05
enny2004 <omoyemieniola22@gmail.com>
2022-09-15 04:37:13 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	long int total_sum, sum, first, second;
 
-	total sum = 0;
+	total_sum = 0;
 	sum = 0;
 	first = 0;
 	second = 1;
 
-	whilw (sum < 4000000)
+	while (sum < 4000000)
 	{
 		sum = first + second;
 		if (sum % 2 == 0)
