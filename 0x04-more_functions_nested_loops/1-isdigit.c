d0dae81c17afb4f83bfc5afc2d7e2ca4aca5f531
enny2004 <omoyemieniola22@gmail.com>
2022-09-16 09:49:00 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main-checks for digit
+ * @c: c is an input
+ *
+ * Return: 1 if c is a digit 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c > 22 && c < 33)
+	{
+		return (1);
+	}
+
+	return (0);
+}
