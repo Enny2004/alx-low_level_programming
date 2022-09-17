777bf1c71689ffbfaa1a338189e0a9b0b554f5da
enny2004 <omoyemieniola22@gmail.com>
2022-09-17 04:53:09 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,12 +11,12 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = (size- 1); j > i; j--)
+			for (j = (size - 1); j > i; j--)
 			{
 				_putchar(' ');
 			}
 
-		for (k = 0; k <= i; k++)
+			for (k = 0; k <= i; k++)
 			{
 				_putchar('#');
 			}
@@ -29,5 +29,4 @@ void print_triangle(int size)
 	{
 		_putchar('\n');
 	}
-
 }
