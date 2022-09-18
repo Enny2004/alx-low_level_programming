df63adbfcf8db8390f9f17bcfb0bbe102c81b03d
enny2004 <omoyemieniola22@gmail.com>
2022-09-18 06:51:00 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,34 +5,29 @@
  * Return:Always (sucessful)
  */
 int main(void)
-(
-	int i, j;
+{
+	int i, j, k;
 
-	for (i = 0; j <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-	
-		for (i = 0; i <= 9; i++)
-		if (j > i)
-
+		for (j = 1; j < 10; j++)
 		{
-			pucwq!
-			thar(i + '0');
-
-			putchar(j + '0');
-
-			if (i != 8)
+			for (k = 2; k < 10; k++)
 			{
-				putchar(',');
-			
-				putchar(' ');
+				if (K > j && j > i)
+				{
+					putchar(i % 10 + '0');
+					Putchar(j % 10 + '0');
+					putchar(k % 10 + '0');
+					if (i != 7 || j != 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
-
-
-
 		}
-
-		putchar('\n');
-
-		return (0);
-		
 	}
+	putchar('\n');
+	return (0);
+}
