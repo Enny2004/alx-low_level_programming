6a3366fdbb0acb292c19b690654682c817e789bd
enny2004 <omoyemieniola22@gmail.com>
2022-09-19 09:30:47 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line
+ *str: take as an argument
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		stdout('\n');
+	}
+}
