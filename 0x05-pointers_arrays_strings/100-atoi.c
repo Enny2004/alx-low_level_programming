22238be8c5e249197e05eb273bd8153713ef0383
enny2004 <omoyemieniola22@gmail.com>
2022-09-19 17:51:32 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,21 +7,39 @@
  */
 int _atoi(char *s)
 {
-	int i, j, k, l;
+	int i, d, n, len, f, digit;
 
-	i = k = 0;
-	l = 1;
-	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-			l *= -1;
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
 		i++;
 	}
-	j = i;
-	while ((s[j] >= '0') && (s[j] <= '9'))
-	{
-		k = (k * 10) + 1 * ((s[j]) - '0');
-		j++;
-	}
-	return (k);
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
