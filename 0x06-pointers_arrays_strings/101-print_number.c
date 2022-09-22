d2066d312faa1ea6e3e733787e563d2a225e3176
enny2004 <omoyemieniola22@gmail.com>
2022-09-22 13:44:24 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,28 +6,18 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int number = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
-	}
-	else
-	{
-		m = n;
-	}
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d / = 10;
-		count *= 10;
+		_putchar('_');
+		number = -n;
 	}
 
-	for (; count >= 1; count / = 10)
+	if (number / 10 != 0)
 	{
-		_putchar(((m / count) % 10) + 48);
+		print_number(number / 10);
 	}
+
+	_putchar(number % 10 + '0')
 }
