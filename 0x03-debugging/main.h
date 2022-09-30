4099317a7ce21db11f06fb13b06a2d799ba348f7
enny2004 <omoyemieniola22@gmail.com>
2022-09-30 02:37:03 -0700

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,11 +6,7 @@
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
-<<<<<<< HEAD
-=======
+void print_remaining_days(int month, int day, int year)
 
 #endif /* MAIN.H */
->>>>>>> f90e8026a3527cba9956aa1a87c31d9b82c6f246
 
-#endif /* MAIN.H */
