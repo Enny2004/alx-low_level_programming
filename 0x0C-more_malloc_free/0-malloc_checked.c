64325b28711de7c13b713f2c0a660c88b3036e7c
enny2004 <omoyemieniola22@gmail.com>
2022-10-07 19:26:44 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,9 @@
 /**
  * malloc_checked - allocates memory
  * @b: amount of bytes
+ *
  * Return: pointers to the allocated memory
+ * if malloc fails, status value equal to 98
  */
 void *malloc_checked(unsigned int b)
 {
