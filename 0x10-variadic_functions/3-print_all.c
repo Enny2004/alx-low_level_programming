1cd9a640694eabab1377e2e4b9c0ea2de47ecbd6
enny2004 <omoyemieniola22@gmail.com>
2022-10-15 07:27:21 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -23,7 +23,7 @@ void print_all(const char * const format, ...)
 				break;
 			} j++;
 		}
-		switch(format[i])
+		switch (format[i])
 		{
 		case 'c':
 			printf("%c", va_arg(valist, int)), c = 1;
